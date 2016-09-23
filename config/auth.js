89758336c1e7dfad00ab19d2920957bb3b9096ae
Corine Lim <blue.yoghurt@gmail.com>
2016-09-23 13:23:18 +0800

    env fix

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : process.env.FB_ID, // your App ID
         'clientSecret'  : process.env.FB_KEY, // your App Secret
-        'callbackURL'   : 'https://inventory-wdi5sg.herokuapp.com/auth/facebook/callback'
+        'callbackURL'   : process.env.FB_URL
     }
 };
 
